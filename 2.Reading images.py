fa2dc561d28dded63cb4a2a81ef4d6335b6b1320
Dawood Sarfraz <dawoodsarfraz.cs@gmail.com>
2024-02-06 14:16:18 +0500

    Update 2.Reading images.py

--- 2.Reading images.py
+++ 2.Reading images.py
@@ -16,13 +16,16 @@ Libraries:
  # !pip install pillow # if not imstalled before
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 image = Image.open("images/cat.jpg")
 # if use Image.open from pillow it's not Numpy array .If you think you can't do image processing bcz of not Numpy array WRONG. you can do Image processing using Pillow function
 print(type(image))
 image.show()
-print("Formate of Image ", image.format)
+print("Formate of Image ", image.format) # will print format of image like jpg, png, tiff etc
 
 # Convert to Numpy
 image1 = np.asanyarray(image)
 print(type(image1))
+plt.imshow(image1)
+print(image1.format)
